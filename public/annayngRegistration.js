8f88e6b3f7f7dc66173f35de0d32b67819d94ee4
Md. Al-amin <38520477+alamin-7@users.noreply.github.com>
2021-03-07 23:27:53 +0600

    Revert "Revert "Annyang JS library added""
    This reverts commit 786f766da26708c5e03bc8ed0a11a522edf59c07.

--- public/annayngRegistration.js
+++ public/annayngRegistration.js
@@ -1,16 +1,41 @@
+
+
+annyang.setLanguage('en-US');
 if (annyang) {
-  alert("Hello");
-  // Let's define our first command. First the text we expect, and then the function it should call
-  var commands = {
-    'enter firstname *tag': function(variable) {
+   // alert("Hello");
+      var commands = {
+     'firstname *tag': function(variable) {
       let firstname = document.getElementById("firstname");
       firstname.value = variable; 
-    }
+      },
+
+     'lastname *tag': function(variable) {
+      let lastname = document.getElementById("lastname");
+      lastname.value = variable; 
+      },
+
+      'username *tag': function(variable) {
+      let username = document.getElementById("username");
+      username.value = variable; 
+      },
+
+     'password *tag': function(variable) {
+      let password = document.getElementById("password");
+      password.value = variable; 
+      },
+
+     'confirm *tag': function(variable) {
+      let password_confirm = document.getElementById("password_confirm");
+      password_confirm.value = variable; 
+      },
+
+      'submit *tag': function() {
+         let registration = document.getElementById("registration");
+         registration.remove();
+      }
   };
 
-  // Add our commands to annyang
   annyang.addCommands(commands);
-
-  // Start listening. You can call this here, or attach this call to an event, button, etc.
   annyang.start();
+  //annyang.start({ autoRestart: true, continuous: false });
 }
